8b2170787162a8ec51b92c6bfbc714cedc988255
Kalle Hirvijärvi <kalle.hirvijarvi@tuni.fi>
2024-10-11 10:26:23 +0300

    implemented get_favorite_flights_by_recent

--- FlightWeatherApp/src/main/java/fi/tuni/java5/flightweatherapp/settingManagement/Favorites.java
+++ FlightWeatherApp/src/main/java/fi/tuni/java5/flightweatherapp/settingManagement/Favorites.java
@@ -1,10 +1,30 @@
 
 package fi.tuni.java5.flightweatherapp.settingManagement;
 
+import fi.tuni.java5.flightweatherapp.Flight;
+import java.util.TreeSet;
+
 /**
  *
  * @author Kalle Hirvijärvi
  */
 public class Favorites {
     
+    public TreeSet<Flight> flights_by_recent;
+    public TreeSet<Flight> flights_by_price;
+    public TreeSet<Flight> flights_by_layover;
+    
+    public Favorites(){
+        flights_by_recent = new TreeSet<Flight>((left, right) -> 
+            left.get_flight_id() - right.get_flight_id()
+        );
+    }
+    
+    public TreeSet<Flight> get_favorite_flights_by_recent(){
+        return flights_by_recent;
+    }
+    
+    public void set_new_favorite(Flight new_flight){
+        flights_by_recent.add(new_flight);
+    }
 }
