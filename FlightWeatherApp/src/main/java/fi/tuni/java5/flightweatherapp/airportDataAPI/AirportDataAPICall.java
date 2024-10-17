b0992ac0ad8c5c143040fabac798300203d31d01
ChuDucAnh242002 <anh.chu@tuni.fi>
2024-10-17 21:32:08 +0300

    Update AirportDataAPICall
    Update AirportDataAPICall. Now can call the API using airport's name or airport's iata's code

--- FlightWeatherApp/src/main/java/fi/tuni/java5/flightweatherapp/airportDataAPI/AirportDataAPICall.java
+++ FlightWeatherApp/src/main/java/fi/tuni/java5/flightweatherapp/airportDataAPI/AirportDataAPICall.java
@@ -10,34 +10,71 @@ import java.util.List;
 import java.lang.reflect.Type;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 
 
 /**
- *
+ * The AirportAPICall class contains method to interact with the Ninja API 
+ * The class get airport-related data such as airport's name, IATA code, latitude, and longitude
+ * 
+ * 
  * @author Chu Duc Anh
  */
 public class AirportDataAPICall {
     private static final String NINJA_API_KEY = "oCFqyGRKgTOJXwbzxwZMIg==zdgkY01L1T5vEVJ9";
     
-    public static List<AirportDataResponse> RequestAirportData(){
+    /**
+     * The function makes a request by airport's name to get the airport's data
+     * 
+     * @param String airport's name default is "Helsinki"
+     * @return List<AirportDataResponse> The list of airport data response related to given airport's name
+     * The airport data response contains of airport's name, IATA code, latitude, and longitude
+     */
+    public static List<AirportDataResponse> RequestAirportDataByAirportName(){
         
-        return getAirportData("Helsinki");
+        return getAirportDataByAirportName("Helsinki");
     }
     
-    public static List<AirportDataResponse> RequestAirportData(String name) {
-        return getAirportData(name);
+    public static List<AirportDataResponse> RequestAirportDataByAirportName(String name) {
+        return getAirportDataByAirportName(name);
     } 
     
-    public static List<AirportDataResponse> getAirportData(String name) {
+    /**
+     * The function makes a request by airport's IATA code to get the airport's data
+     * 
+     * @param String airport's IATA code, default is "HEL
+     * @return List<AirportDataResponse> The list of airport data response related to given airport's IATA code
+     * The airport data response contains of airport's name, IATA code, latitude, and longitude
+     */
+    public static List<AirportDataResponse> RequestAirportDataByAirportCode() {
+        return getAirportDataByAirportCode("HEL");
+    }
+    
+    public static List<AirportDataResponse> RequestAirportDataByAirportCode(String iata) {
+        return getAirportDataByAirportCode(iata);
+    }
+    
+    private static List<AirportDataResponse> getAirportDataByAirportName(String name) {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://api.api-ninjas.com/v1/airports?name=" + name))
                 .header("X-Api-Key", NINJA_API_KEY)
                 .method("GET", HttpRequest.BodyPublishers.noBody())
 		.build();
         
+        return getAirportData(request);
+    }
+    
+    private static List<AirportDataResponse> getAirportDataByAirportCode(String iata) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.api-ninjas.com/v1/airports?iata=" + iata))
+                .header("X-Api-Key", NINJA_API_KEY)
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+		.build();
+        
+        return getAirportData(request);
+    }
+    
+    private static List<AirportDataResponse> getAirportData(HttpRequest request) {
         try {
             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
             
