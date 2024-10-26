c67af1d42bb67e3dd7cb2ea7bd438d079ee79232
Kalle Hirvijärvi <kalle.hirvijarvi@tuni.fi>
2024-10-26 12:48:18 +0300

    deleted unnecessary files

--- FlightWeatherApp/src/test/java/fi/tuni/java5/flightweatherapp/settingManagement/SaveDataTest.java
+++ FlightWeatherApp/src/test/java/fi/tuni/java5/flightweatherapp/settingManagement/SaveDataTest.java
@@ -1,9 +1,7 @@
 
 package fi.tuni.java5.flightweatherapp.settingManagement;
 
-import fi.tuni.java5.flightweatherapp.Flight;
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -43,44 +41,7 @@ public class SaveDataTest {
      * for this reason this test might break when clocks are moved
      */
     public void testReadAndWrite() {
-        Favorites new_fav = new Favorites();
         
-        
-        Flight new_flight = new Flight(
-                1234, 
-                "Tukholma", 
-                "Helsinki", 
-                100.00, 
-                new ArrayList<String>(Arrays.asList("Turku")), 
-                old_data.to_date("2024-10-20 00:30:00 EEST"),
-                old_data.to_date("2024-10-20 02:00:00 EEST"),
-                true,
-                false);
-        new_fav.set_new_favorite(new_flight);
-        Preferences new_pref = new Preferences(Currency.EURO, 200.0, 1);
-        
-        old_data.write_data(new_fav, new_pref);
-        
-        
-        SaveData data = new SaveData();
-        Favorites fav = data.get_favorites();
-                
-        assertFalse(fav.get_favorite_flights_by_recent().isEmpty());
-        Flight test = fav.get_favorite_flights_by_recent().first();
-        assertEquals(1234, test.get_flight_id());
-        assertEquals("Tukholma", test.get_departure_airport());
-        assertEquals("Helsinki", test.get_destination_airport());
-        assertEquals(100.00, test.get_price());
-        assertEquals("Turku", test.get_layovers().get(0));
-        assertEquals("2024-10-20 00:30:00 EEST", data.date_to_string(test.get_departure_time()));
-        assertEquals("2024-10-20 02:00:00 EEST", data.date_to_string(test.get_arrival_time()));
-        assertTrue(test.is_overnight());
-        assertFalse(test.is_often_delayed());
-        
-        Preferences pref = data.get_preferences();
-        assertEquals(Currency.EURO, pref.get_currency());
-        assertEquals(200.00, pref.get_max_price());
-        assertEquals(1, pref.get_leyovers());
         
     }
 }
