404860c6253f094fb97e24227a5c2230f3aa5700
Kalle Hirvijärvi <kalle.hirvijarvi@tuni.fi>
2024-11-17 14:18:31 +0200

    some minor improvements

--- FlightWeatherApp/src/main/java/fi/tuni/java5/flightweatherapp/settingManagement/Preferences.java
+++ FlightWeatherApp/src/main/java/fi/tuni/java5/flightweatherapp/settingManagement/Preferences.java
@@ -10,17 +10,36 @@ package fi.tuni.java5.flightweatherapp.settingManagement;
 public class Preferences {
     
     private String currency_;
-    private Double max_price_;
-    private int layovers_;
+    private String temp_unit;   // unit of temperature
+    private Double max_price_;  
+    private int layovers_;      // max number of layovers
     
-    
-    public Preferences(String currency, Double max_price, int layovers){
+    /**
+     * paramaters set object attributes
+     * @param currency
+     * @param temperature_unit
+     * @param max_price
+     * @param layovers 
+     * @throws IllegalArgumentException when currency or temperature unit is not valid
+     */
+    public Preferences(String currency, String temperature_unit, Double max_price, int layovers){
         currency_ = currency;
+        temp_unit = temperature_unit;
         max_price_ = max_price;
         layovers_ = layovers;
+        if (!validate_currency(currency)){
+            throw new IllegalArgumentException();
+        }
+        if (!validate_temperature_unit(temperature_unit)){
+            throw new IllegalArgumentException();
+        }
     }
+    // getters
     public String get_currency(){
         return currency_;
+    }
+    public String get_temperature_unit(){
+        return temp_unit;
     }
     public Double get_max_price(){
         return max_price_;
@@ -29,14 +48,44 @@ public class Preferences {
         return layovers_;
     }
     
-    public void set_currency(String currency){
-        currency_ = currency;
+    // setters for currency and temperature unit setters validate
+    // the parameter and return boolean value:
+    // valid: true
+    // invalid: false
+    public boolean set_currency(String currency){
+        if (validate_currency(currency)){
+            currency_ = currency;
+            return true;
+        }
+        return false;
     }
+    public boolean set_temperature_unit(String temperature_unit){
+        if (validate_temperature_unit(temperature_unit)){
+            temp_unit = temperature_unit;
+            return true;
+        }
+        return false;
+    }
+    
+    // other setters
     public void set_max_price(Double max_price){
         max_price_ = max_price;
     }
     public void set_layovers(int layovers){
         layovers_ = layovers;
+    }   
+    /**
+     * @param input
+     * @return true if input is "C" or "F"
+     */
+    private boolean validate_temperature_unit(String input){
+        return input.compareTo("C") == 0 || input.compareTo("F") == 0;
     }
-   
+    /**
+     * @param input
+     * @return true of input is "EUR" or "USD" or "GBP"
+     */
+    private boolean validate_currency(String input){
+        return input.compareTo("EUR") == 0 || input.compareTo("USD") == 0 || input.compareTo("GBP") == 0;
+    }
 }
--- FlightWeatherApp/src/main/java/fi/tuni/java5/flightweatherapp/settingManagement/SaveData.java
+++ FlightWeatherApp/src/main/java/fi/tuni/java5/flightweatherapp/settingManagement/SaveData.java
@@ -17,7 +17,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Reads and wites data for Favorites and Preferences objects to and from a JSON file.
@@ -64,6 +63,7 @@ public class SaveData {
      */
     private void parse_json_object(JsonObject data){
         
+        // get favorites and save them to Favorites object
         for (SearchResultCard new_result : read_flights_from_array(data.get("favorites").getAsJsonArray())){
             favorites.set_new_favorite(new_result);
         }
@@ -72,13 +72,25 @@ public class SaveData {
         
         // read all indivitua properties from preferences JSON object
         String currency = pref.get("currency").getAsString();
+        String temperature_unit = pref.get("temp_unit").getAsString();
         Double max_price = pref.get("max_price").getAsDouble();
         int number_of_layovers = pref.get("num_of_layovers").getAsInt();
         
         // and save it to new preferences object
-        preferences = new Preferences(currency, max_price, number_of_layovers);
+        try {
+            preferences = new Preferences(currency, temperature_unit, max_price, number_of_layovers);
+        } catch(IllegalArgumentException e){
+            // if currency or temperature is invalid, create new object with
+            // default preferences
+            preferences = new Preferences("EUR", "C", -1.0, -1);
+        }
         
     }
+    /**
+     * converts correctly formatted JsonArray of SearchResultCards to List
+     * @param input JsonArray
+     * @return resulting List
+     */
     private List<SearchResultCard> read_flights_from_array(JsonArray input){
         
         List<SearchResultCard> result = new ArrayList<>();
@@ -88,10 +100,11 @@ public class SaveData {
             JsonObject current = flight_card_json.getAsJsonObject();
             
             List<Flight> flight_list = new ArrayList<>();
-            // read all indivitual properties from the flight JSON object
+            // read all indivitual properties from Json file and save Flight obejct into list
             for (JsonElement flight_json : current.get("flights").getAsJsonArray()){
                 JsonObject current_flight = flight_json.getAsJsonObject();
                 
+                // get data for Flight object from Json file
                 String dep_airport_name = current_flight.get("dep_airport_name").getAsString();
                 String dep_airport_id = current_flight.get("dep_airport_id").getAsString();
                 String dep_airport_time = current_flight.get("dep_airport_time").getAsString();
@@ -100,28 +113,34 @@ public class SaveData {
                 String arr_airport_time = current_flight.get("arr_airport_time").getAsString();
                 int flight_duration = current_flight.get("duration").getAsInt();
                 String airplane = current_flight.get("airplane").getAsString();
+                String air_line = current_flight.get("airline").getAsString();
                 String airline_logo = current_flight.get("airline_logo").getAsString();
                 String travel_class = current_flight.get("class").getAsString();
                 String flight_number = current_flight.get("flight_num").getAsString();
                 List<String> tickets_sold_by = JSONArray_to_List(current_flight.get("tickets_sold_by").getAsJsonArray());
                 List<String> extentions = JSONArray_to_List(current_flight.get("extentions").getAsJsonArray());
                 
+                // create new internal obejcts
                 Flight new_flight = new Flight();
                 Airport dep_airport = new Airport();
                 Airport arr_airport = new Airport();
                 
+                // populate with data (departure airport)
                 dep_airport.name = dep_airport_name;
                 dep_airport.id = dep_airport_id;
                 dep_airport.time = dep_airport_time;
                 new_flight.departure_airport = dep_airport;
                 
+                // populate with data (arrival airport)
                 arr_airport.name = arr_airport_name;
                 arr_airport.id = arr_airport_id;
                 arr_airport.time = arr_airport_time;
                 new_flight.arrival_airport = arr_airport;
                 
+                // populate with data (flight)
                 new_flight.duration = flight_duration;
                 new_flight.airplane = airplane;
+                new_flight.airline = air_line;
                 new_flight.airline_logo = airline_logo;
                 new_flight.travel_class = travel_class;
                 new_flight.flight_number = flight_number;
@@ -130,14 +149,17 @@ public class SaveData {
                 
                 flight_list.add(new_flight);
             }
+            // read all indivitual properties from Json file and save Layover obejct into list
             List<Layover> layover_list = new ArrayList<>();
             for (JsonElement layover_json : current.get("layovers").getAsJsonArray()){
                 JsonObject current_layover = layover_json.getAsJsonObject();
                 
+                // get properties from Json file
                 int l_duration = current_layover.get("duration").getAsInt();
                 String layover_name = current_layover.get("name").getAsString();
                 String layover_id = current_layover.get("layover_id").getAsString();
                 
+                // create new object and populate with data
                 Layover new_layover = new Layover();
                 new_layover.duration = l_duration;
                 new_layover.name = layover_name;
@@ -147,6 +169,7 @@ public class SaveData {
                 
             }
             
+            // read data fro SearchResultCard from Json file
             int total_duration = current.get("total_duration").getAsInt();
             int carbon_emissions = current.get("emissions").getAsInt();
             double  price = current.get("price").getAsDouble();
@@ -154,10 +177,9 @@ public class SaveData {
             String airline_logo = current.get("airline_logo").getAsString();
             String departure_token = current.get("dep_token").getAsString();
             
-            
-            // create Flight obejct
             SearchResultCard new_flight = new SearchResultCard();
             
+            // populate final object with data
             new_flight.flights = flight_list;
             new_flight.layovers = layover_list;
             
@@ -168,13 +190,16 @@ public class SaveData {
             new_flight.airline_logo = airline_logo;
             new_flight.departure_token = departure_token;
             
-            // and save it to Favorites object
             result.add(new_flight);
         }        
         return result;
         
     }
-    
+    /**
+     * converts list of SerachResultCard objects into correctly formatted JsonArray
+     * @param input List of of SerachResultCard objects
+     * @return result JsonArray
+     */
     private JsonArray write_flights_to_JSONArray(List<SearchResultCard> input){
         
         JsonArray result = new JsonArray();
@@ -186,16 +211,20 @@ public class SaveData {
             for (Flight flight : result_card.getFlights()){
                 JsonObject flight_json = new JsonObject();
                 
+                // departure airport properties
                 flight_json.addProperty("dep_airport_name", flight.getDepartureAirport().name);
                 flight_json.addProperty("dep_airport_id", flight.getDepartureAirport().id);
                 flight_json.addProperty("dep_airport_time", flight.getDepartureAirport().time);
                 
+                // arrival airport properties
                 flight_json.addProperty("arr_airport_name", flight.getArrivalAirport().name);
                 flight_json.addProperty("arr_airport_id", flight.getArrivalAirport().id);
                 flight_json.addProperty("arr_airport_time", flight.getArrivalAirport().time);
                 
+                // flight properties
                 flight_json.addProperty("duration", flight.getDuration());
                 flight_json.addProperty("airplane", flight.getAirplane());
+                flight_json.addProperty("airline", flight.getAirline());
                 flight_json.addProperty("airline_logo", flight.getAirlineLogo());
                 flight_json.addProperty("class", flight.getTravelClass());
                 flight_json.addProperty("flight_num", flight.getFlightNumber());
@@ -209,6 +238,7 @@ public class SaveData {
             for (Layover layover : result_card.getLayovers()){
                 JsonObject layover_json = new JsonObject();
                 
+                // layover properties
                 layover_json.addProperty("duration", layover.getDuration());
                 layover_json.addProperty("name", layover.getName());
                 layover_json.addProperty("layover_id", layover.getId());
@@ -218,8 +248,9 @@ public class SaveData {
             result_card_json.add("flights", flights);
             result_card_json.add("layovers", layovers);
             
+            // indivitual FlightResultCard properties
             result_card_json.addProperty("total_duration", result_card.getTotalDuration());
-            result_card_json.addProperty("emissions", result_card.getTotalDuration());
+            result_card_json.addProperty("emissions", result_card.carbon_emission);
             result_card_json.addProperty("price", result_card.getPrice());
             result_card_json.addProperty("type", result_card.getType());
             result_card_json.addProperty("airline_logo", result_card.getAirlineLogo());
@@ -275,23 +306,23 @@ public class SaveData {
      */
     public void write_data(Favorites fav, Preferences pref){
         
-        // I don't know how this works but this is needed here
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        
         // saves preferences to JSON object
         JsonObject pref_result = new JsonObject();
         pref_result.addProperty("currency", pref.get_currency());
+        pref_result.addProperty("temp_unit", pref.get_temperature_unit());
         pref_result.addProperty("max_price", pref.get_max_price());
         pref_result.addProperty("num_of_layovers", pref.get_leyovers());
         
         JsonObject result = new JsonObject();
-        result.add("favorites", write_flights_to_JSONArray(favorites.get_favorite_flights_by_dep_time()));
+        result.add("favorites", write_flights_to_JSONArray(fav.get_favorite_flights_by_dep_time()));
         result.add("preferences", pref_result);
         
         // writes result to JSON file        
         try (FileWriter writer = new FileWriter(file_name)) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
             String jsonString = gson.toJson(result);
             writer.write(jsonString);
+            writer.close();
         }
         catch (IOException e){
             System.err.println("target file not found: " +  e);
